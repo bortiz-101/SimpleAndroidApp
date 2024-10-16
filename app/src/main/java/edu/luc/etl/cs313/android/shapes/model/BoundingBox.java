958622d038227f8992c3be7002d135f20decc7f8
bortiz-101 <103912210+bortiz-101@users.noreply.github.com>
2024-10-15 21:09:19 -0500

    completes BoundingBox

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -1,5 +1,13 @@
 package edu.luc.etl.cs313.android.shapes.model;
 
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static edu.luc.etl.cs313.android.shapes.model.Fixtures.simpleGroup;
+
 /**
  * A shape visitor for calculating the bounding box, that is, the smallest
  * rectangle containing the shape. The resulting bounding box is returned as a
@@ -17,55 +25,92 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-       Shape shape = f.getShape();
-       final var a = shape.accept(this);
-       final var b = (Rectangle) a.getShape();
-       final int x = a.getX();
-       final int y = a.getY();
-       final int w = b.getWidth();
-       final int h = b.getHeight();
-       return new Location(x, y, new Rectangle(w, h));
+      return f.getShape().accept(this);
     }
 
     @Override
     public Location onGroup(final Group g) {
-    return null;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
 
+        for(Shape shapes : g.getShapes()) {
+            Location location = shapes.accept(this);
+            if(location != null) {
+                Shape shape = location.getShape();
+                int x = location.getX();
+                int y = location.getY();
+
+                if(shape instanceof Rectangle) {
+                    Rectangle r = (Rectangle) shape;
+                    minX = Math.min(minX, x);
+                    minY = Math.min(minY, y);
+                    maxX = Math.max(maxX, x + r.getWidth());
+                    maxY = Math.max(maxY, y + r.getHeight());
+                }
+                else if (shape instanceof Circle) {
+                    Circle c = (Circle) shape;
+                    minX = Math.min(minX, x - c.getRadius());
+                    minY = Math.min(minY, y - c.getRadius());
+                    maxX = Math.max(maxX, x + c.getRadius());
+                    maxY = Math.max(maxY, y + c.getRadius());
+                } else if (shape instanceof Polygon) {
+                    Location polyLocation = onPolygon((Polygon) shape);
+                    minX = Math.min(minX, polyLocation.getX());
+                    minY = Math.min(minY, polyLocation.getY());
+                    Rectangle polyBounds = (Rectangle) polyLocation.getShape();
+                    maxX = Math.max(maxX, polyLocation.getX() + polyBounds.getWidth());
+                    maxY = Math.max(maxY, polyLocation.getY() + polyBounds.getHeight());
+                }
+            }
+        }
+        return new Location(minX, minY, new Rectangle(maxX- minX, maxY - minY));
     }
 
     @Override
     public Location onLocation(final Location l) {
-        final int x = l.getX();
-        final int y = l.getY();
-        final var a = (Rectangle) l.getShape();
-        final int w = a.getWidth();
-        final int h = a.getHeight();
-        return new Location(x, y, new Rectangle(w,h));
+        Location a = l.getShape().accept(this);
+        Shape shape = a.getShape();
+        int x = a.getX() + l.getX();
+        int y = a.getY() + l.getY();
+        if (shape instanceof Rectangle) {
+            Rectangle rect = (Rectangle) shape;
+            return new Location(x,y, rect);
+        }
+        return null;
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        final int width = r.getWidth();
-        final int height = r.getHeight();
-        return new Location(0,0, new Rectangle(width, height));
+        return new Location(0,0, r);
     }
 
     @Override
     public Location onStrokeColor(final StrokeColor c) {
-        final var a = (Rectangle) c.getShape();
-        final int h = a.getHeight();
-        final int w = a.getWidth();
-        return new Location(0,0, new Rectangle(w,h));
-
+        return c.getShape().accept(this);
     }
 
     @Override
     public Location onOutline(final Outline o) {
-        return o.accept(this);
+        return o.getShape().accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
+        List<? extends Point> points = s.getPoints();
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (Point point : points) {
+            minX = Math.min(minX, point.getX());
+            minY = Math.min(minY, point.getY());
+            maxX = Math.max(maxX, point.getX());
+            maxY = Math.max(maxY, point.getY());
+
+        }
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
     }
 }
--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
@@ -1,5 +1,7 @@
 package edu.luc.etl.cs313.android.shapes.model;
 
+import android.telephony.TelephonyManager;
+
 /**
  * A point, implemented as a location without a shape.
  */
@@ -7,12 +9,9 @@ public class Point extends Location {
 
     // TODO your job
     // HINT: use a circle with radius 0 as the shape!
-    public Shape getShape() {
-        return new Circle(0);
-    }
 
     public Point(final int x, final int y) {
-        super(-1, -1, null);
+        super(x, y, new Circle(0));
         assert x >= 0;
         assert y >= 0;
     }
